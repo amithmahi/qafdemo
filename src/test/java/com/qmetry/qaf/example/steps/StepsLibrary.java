bc23d05bafc4e8377ad0862fcede4a3e95a42556
Hiten Prajapati <hiten.prajapati@infostretch.com>
2018-03-08 18:50:04 +0530

    git project update

--- src/test/java/com/qmetry/qaf/example/steps/StepsLibrary.java
+++ src/test/java/com/qmetry/qaf/example/steps/StepsLibrary.java
@@ -3,6 +3,8 @@ package com.qmetry.qaf.example.steps;
 import static com.qmetry.qaf.automation.step.CommonStep.click;
 import static com.qmetry.qaf.automation.step.CommonStep.sendKeys;
 
+import org.openqa.selenium.Keys;
+
 import com.qmetry.qaf.automation.step.QAFTestStep;
 
 public class StepsLibrary {
@@ -12,7 +14,7 @@ public class StepsLibrary {
 	 */
 	@QAFTestStep(description = "search for {0}")
 	public static void searchFor(String searchTerm) {
-		sendKeys(searchTerm, "input.search");
+		sendKeys(searchTerm+Keys.TAB, "input.search");
 		click("button.search");
 	}
 }
--- src/test/java/com/qmetry/qaf/example/test/SampleTest.java
+++ src/test/java/com/qmetry/qaf/example/test/SampleTest.java
@@ -20,39 +20,4 @@ public class SampleTest extends WebDriverTestCase {
 		searchFor("qaf github infostretch");
 		verifyLinkWithPartialTextPresent("qaf");
 	}
-
-	@Test(groups = { "P1", "storykey=ZAL-1" })
-	public void testGoogleSearch41() {
-		get("/");
-		searchFor("qaf github infostretch");
-		verifyLinkWithPartialTextPresent("qaf");
-	}
-
-	@Test(groups = { "P1", "storykey=ZAL-1" })
-	public void testGoogleSearch1() {
-		get("/");
-		searchFor("qaf github infostretch");
-		verifyLinkWithPartialTextPresent("qaf");
-	}
-
-	@Test(groups = { "P1", "storykey=ZAL-1" })
-	public void testGoogleSearch2() {
-		get("/");
-		searchFor("qaf github infostretch");
-		verifyLinkWithPartialTextPresent("qaf");
-	}
-
-	@Test(groups = { "P1", "storykey=ZAL-1" })
-	public void testGoogleSearch3() {
-		get("/");
-		searchFor("qaf github infostretch");
-		verifyLinkWithPartialTextPresent("qaf");
-	}
-
-	@QAFDataProvider(key = "data.test")
-	@Test(groups = { "P1", "storykey=ZAL-1" })
-	public void testGoogleSearch5(String name) {
-		System.out.println(name);
-	}
-
 }
